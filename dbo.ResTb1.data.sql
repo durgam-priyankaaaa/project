4435440189e9cbd28eb6aa55a86a1e2999bf7f56
durgam-priyankaaaa <115684685+durgam-priyankaaaa@users.noreply.github.com>
2022-10-13 10:40:35 +0530

    Add files via upload

---
+++ dbo.ResTb1.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[ResTb1] ([RNum], [Student], [Algo], [CProg], [Java], [DBMS], [Python], [Total], [Percentage], [Decision]) VALUES (1, N'St001', 55, 67, 78, 34, 55, 289, 192, N'Pass')
